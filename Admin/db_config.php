271722fc58863f59526ddd387d504f928f849c83
Razib Hasan <razibhasan2403@gmail.com>
2025-12-07 13:49:49 +0600

    7 dec, cw

---
+++ Admin/db_config.php
@@ -0,0 +1,10 @@
+<?php
+
+$conn = new mysqli(
+    'localhost',    // Database host
+    'root',      // Database username
+    '',  // Database password
+    'hotel'       // Database name
+)
+
+?>
